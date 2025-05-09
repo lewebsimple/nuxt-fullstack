0efa53a4751a258295e14ebc095640e80b1a8174
Pascal Martineau <pascal@lewebsimple.ca>
2025-05-09 09:16:10 -0400

    chore: Initial Nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  modules: ["@nuxt/eslint"],
+
+  devtools: { enabled: true },
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: "2024-11-01",
+
+  eslint: {
+    config: {
+      stylistic: {
+        arrowParens: true,
+        commaDangle: "always-multiline",
+        indent: 2,
+        quotes: "double",
+        semi: true,
+      },
+    },
+  },
+});
