4961b3e430176347242079efd5dd360c3727cf87
Pascal Martineau <pascal@lewebsimple.ca>
2025-05-19 14:20:46 -0400

    minor: appVersion should not be nullable

--- server/graphql/types/app.ts
+++ server/graphql/types/app.ts
@@ -2,5 +2,5 @@ import { version } from "~~/package.json";
 
 // App queries
 export const AppQueries = builder.queryFields((t) => ({
-  appVersion: t.field({ type: "String", resolve: () => version }),
+  appVersion: t.field({ type: "String", nullable: false, resolve: () => version }),
 }));
